5ae7c38218536e612eb2c85ce0580ac96132c4e6
unknown <vladfilyushkin@gmail.com>
2024-05-28 17:57:00 +0300

    убрал лишнее

--- src/homework4.java
+++ src/homework4.java
@@ -8,37 +8,11 @@ public class homework4 {
         float x2 = scanner.nextFloat();
         float y2 = scanner.nextFloat();
 
-        if(x1<0){
-            x1=Math.abs(x1);
-
-        }
-        if(x2<0){
-            x2=Math.abs(x2);
-
-        }
-        if(y1<0){
-            y1=Math.abs(y1);
-
-        }
-
-        if(y2<0){
-            y2=Math.abs(y2);
-
-        }
         if(x1==x2 && y1==y2) {
-            System.out.println("Ошибка");
-        } else if (x2==x1) {
-            System.out.print("Длина отрезка: "+(y2-y1));
-        } else if (y2==y1) {
-            System.out.print("Длина отрезка: "+(x2-x1));
+            System.out.println("Ошибка, отрезок является точкой");
         } else {
             System.out.print("Длина отрезка: "+ Math.sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1)));
         }
-
-
-
-
-
 
     }
 }
